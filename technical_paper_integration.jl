74fe335f7c1c52a29661407204a578dd73606551
William S. Moses <gh@wsmoses.com>
2024-12-03 22:18:04 -0500

    cleaning

--- technical_paper_integration.jl
+++ technical_paper_integration.jl
@@ -9,14 +9,32 @@ include("../ShallowWaters.jl/src/ShallowWaters.jl")
 using .ShallowWaters
 
 using Enzyme
-using Checkpointing, HDF5, Serialization
-using NetCDF, JLD2, CairoMakie
+using Checkpointing
 
-Enzyme.API.looseTypeAnalysis!(true)
+mutable struct MyPrognosticVars{T<:AbstractFloat}
+    u::Array{T,2}           # u-velocity
+    nu::Array{T,2}           # sea surface height / interface displacement
+end
 
-using Parameters
-using Optim
-using LaTeXStrings
+function loop(S,scheme)
+    @checkpoint_struct scheme S for S.parameters.i = 1:S.grid.nt
+        Diag = S.Diag
+        Prog = S.Prog
+
+        halo = S.grid.halo
+
+        # undo scaling as well
+        @views ucut = S.Prog.u[halo+1:end-halo,halo+1:end-halo]
+        ηcut = S.Prog.η
+
+        temp = MyPrognosticVars{Float64}(ucut, ηcut) 
+
+        energy_lr = first(temp.u.^2)
+
+        S.parameters.J += energy_lr
+    end
+    return nothing
+end
 
 function checkpointed_integration(S, scheme)
     h = S.Diag.VolumeFluxes.h
@@ -32,62 +50,3 @@ function checkpointed_integration(S, scheme)
 
 end
 
-function myremove_halo(   u::Array{T,2},
-                        v::Array{T,2},
-                        η::Array{T,2},
-                        sst::Array{T,2},
-                        S) where {T<:AbstractFloat}
-
-    @unpack halo,haloη,halosstx,halossty = S.grid
-
-    # undo scaling as well
-    @views ucut = u[halo+1:end-halo,halo+1:end-halo]
-    ηcut = η
-
-    return ucut,ηcut
-end
-
-mutable struct MyPrognosticVars{T<:AbstractFloat}
-    u::Array{T,2}           # u-velocity
-    nu::Array{T,2}           # sea surface height / interface displacement
-end
-
-function loop(S,scheme)
-
-
-    @checkpoint_struct scheme S for S.parameters.i = 1:S.grid.nt
-
-        Diag = S.Diag
-        Prog = S.Prog
-
-        # if S.parameters.i in (S.grid.nt - 30*224):1:S.grid.nt
-
-            temp = MyPrognosticVars{Float64}(myremove_halo(S.Prog.u,
-            S.Prog.v,
-            S.Prog.η,
-            S.Prog.sst,S)...)
-
-            energy_lr = first(temp.u.^2)
-
-            S.parameters.J += energy_lr
-
-        # end
-
-    end
-
-    ##### use if time-averaging the objective function #######
-    ##########################################################
-
-    ##### Energy objective function, not time averaged ###########
-    # temp = ShallowWaters.PrognosticVars{Float32}(ShallowWaters.remove_halo(S.Prog.u,
-    # S.Prog.v,
-    # S.Prog.η,
-    # S.Prog.sst,S)...)
-
-    # energy_lr = (sum(temp.u.^2) + sum(temp.v.^2)) / (S.grid.nx * S.grid.ny)
-    # S.parameters.J = energy_lr
-    ###########################################
-
-    return nothing
-
-end
