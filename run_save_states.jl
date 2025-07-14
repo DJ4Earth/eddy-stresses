7343d896685483ec6c47c81554f4e27cb1d97068
Sarah Williamson <s.miles.williamson@gmail.com>
2025-07-14 12:49:50 -0500

    saving more information from the high resolution

---
+++ run_save_states.jl
@@ -0,0 +1,24 @@
+include("eddy_paper.jl")
+
+Shr = ShallowWaters.model_setup(output=false,
+        L_ratio=1,
+        g=9.81,
+        H=500,
+        wind_forcing_x="double_gyre",
+        Lx=3840e3,
+        seasonal_wind_x=false,
+        topography="flat",
+        bc="nonperiodic",
+        bottom_drag="quadratic",
+        tracer_advection=false,
+        tracer_relaxation=false,
+        zb_forcing_momentum=false,
+        zb_forcing_dissipation=false,
+        zb_filtered=true,
+        nn_forcing_momentum=false,
+        nn_forcing_dissipation=false,
+        N=1,
+        α=2,
+        nx=1024,
+        Ndays=30
+)
