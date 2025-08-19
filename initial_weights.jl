edbd7369e1723613b6eaa7f3da2ede315a7cce6e
Sarah Williamson <swilliamson@utexas.edu>
2025-08-19 13:26:14 -0500

    .

--- initial_weights.jl
+++ initial_weights.jl
@@ -19,7 +19,8 @@ function for_enzyme(param_guess, state, SNN, SZB)
 
     # return sum((SZB.Diag.ZBVars.S_u .- SNN.Diag.NNVars.S_u).^2) ./ (128*127) #+ sum((SZB.Diag.ZBVars.S_v .- SNN.Diag.NNVars.S_v).^2) ./ (128*127)
     # return sum((SZB.Diag.ZBVars.S_u[50:52,50] - SNN.Diag.NNVars.S_u[50:52,50]).^2)
-    return (SZB.Diag.ZBVars.S_u[50,50] - SNN.Diag.NNVars.T11[50,50]).^2
+    temp = reshape(collect(1:36), 6, 6)
+    return sum((temp - SNN.Diag.NNVars.T11[40:45,40:45]).^2)
 end
 
 function initweights_compute_loss(param_guess, state)
@@ -84,7 +85,8 @@ function initweights_compute_loss(param_guess, state)
 
     # return sum((SZB.Diag.ZBVars.S_u .- SNN.Diag.NNVars.S_u).^2) ./ (128*127) #+ sum((SZB.Diag.ZBVars.S_v .- SNN.Diag.NNVars.S_v).^2) ./ (128*127)
     # return sum((SZB.Diag.ZBVars.S_u[50:52,50] - SNN.Diag.NNVars.S_u[50:52,50]).^2)
-    return (SZB.Diag.ZBVars.S_u[50,50] - SNN.Diag.NNVars.T11[50,50]).^2
+    temp = reshape(collect(1:36), 6, 6)
+    return sum((temp - SNN.Diag.NNVars.T11[40:45,40:45]).^2)
 
 end
 
@@ -189,7 +191,7 @@ function compute_init_weights()
     vlr = ncread("./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825/v.nc", "v")
     etalr = ncread("./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825/eta.nc", "eta")
 
-    param_guess = 1000 .* randn(1357+1691)
+    param_guess = randn(5442+5521)
 
     result = nothing
     for j in [10]
@@ -197,7 +199,7 @@ function compute_init_weights()
         u, v, _ = ShallowWaters.add_halo(ulr[:,:,j], vlr[:,:,j], etalr[:,:,j], S)
         fg!_closure(F, G, param_guess) = initweights_FG(F, G, param_guess, [u, v])
         obj_fg = Optim.only_fg!(fg!_closure)
-        result = Optim.optimize(obj_fg, param_guess, Optim.Options(show_trace=true, store_trace=true))
+        result = Optim.optimize(obj_fg, param_guess, Optim.Adam(; alpha = .1), Optim.Options(show_trace=true, store_trace=true, iterations=1500))
         param_guess = result.minimizer
 
     end
