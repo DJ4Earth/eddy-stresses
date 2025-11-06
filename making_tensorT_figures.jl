fc83042a019f089a5d19c6fb94fb4aa750f23a30
Sarah Williamson <s.miles.williamson@gmail.com>
2025-11-06 17:34:23 -0600

    maybe have online working

--- making_tensorT_figures.jl
+++ making_tensorT_figures.jl
@@ -1,9 +1,36 @@
-function compute_Ts()
+function compute_Ts(j, Ndays)
 
     ulr = ncread("./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825/u.nc", "u")
     vlr = ncread("./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825/v.nc", "v")
     etalr = ncread("./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825/eta.nc", "eta")
-    j = 1
+
+    Ppred = ShallowWaters.Parameter(T=Float64;
+        output=false,
+        L_ratio=1,
+        g=9.81,
+        H=500,
+        wind_forcing_x="double_gyre",
+        Lx=3840e3,
+        seasonal_wind_x=false,
+        topography="flat",
+        bc="nonperiodic",
+        bottom_drag="quadratic",
+        tracer_advection=false,
+        tracer_relaxation=false,
+        zb_forcing_momentum=false,
+        zb_forcing_dissipation=false,
+        zb_filtered=true,
+        nn_forcing_momentum=false,
+        nn_forcing_dissipation=true,
+        N=1,
+        α=2,
+        nx=128,
+        Ndays=Ndays,
+        initial_cond="ncfile",
+        initpath="./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825",
+        init_starti=1
+    );
+    Spred = ShallowWaters.model_setup(Ppred);
 
     PNN = ShallowWaters.Parameter(T=Float64;
         output=false,
@@ -25,7 +52,11 @@ function compute_Ts()
         nn_forcing_dissipation=true,
         N=1,
         α=2,
-        nx=128
+        nx=128,
+        Ndays=Ndays,
+        initial_cond="ncfile",
+        initpath="./spinup_files/128_postspinup_noforcing_cginitcondition_oneyear_071825",
+        init_starti=1
     );
     SNN = ShallowWaters.model_setup(PNN);
 
@@ -33,9 +64,10 @@ function compute_Ts()
     snapshot = [u,v]
 
     # param_guess = load_object("./offline_files/offlineresult_3-25-25_1e-3objective_relu_activation.jld2").solution;
-    param_guess = load_object("./offline_files/offlineresult_geluactivation_1e-5obj_300iterations_110525.jld2").solution;
+    # param_guess = load_object("./offline_files/offlineresult_geluactivation_1e-5obj_300iterations_110525.jld2").solution;
     # param_guess = load_objecT("./offline_files/offline")
     # param_guess2 = zeros(Lux.parameterlength(SNN.Diag.CNNVars.model_Su) + Lux.parameterlength(SNN.Diag.CNNVars.model_Sv))
+    param_guess = load_object("./result_optim_toview.jld2").minimizer;
     current = 1
     for model in (SNN.Diag.CNNVars.model_Su, SNN.Diag.CNNVars.model_Sv)
         for layers in model[1]
@@ -48,7 +80,14 @@ function compute_Ts()
         end
     end
 
-    ShallowWaters.CNN_momentum(snapshot[1], snapshot[2], SNN);
+    ShallowWaters.time_integration(Spred)
+    ShallowWaters.time_integration(SNN)
+    # ShallowWaters.CNN_momentum(snapshot[1], snapshot[2], Spred);
+    # ShallowWaters.CNN_momentum(snapshot[1], snapshot[2], SNN);
+
+    T11_pred = Spred.Diag.CNNVars.T11;
+    T12_pred = Spred.Diag.CNNVars.T12;
+    T22_pred = Spred.Diag.CNNVars.T22;
 
     T11_NN = SNN.Diag.CNNVars.T11;
     T12_NN = SNN.Diag.CNNVars.T12;
@@ -122,11 +161,7 @@ function compute_Ts()
     ζDhat_filtered = SZB.Diag.ZBVars.ζDhat_filtered;
     trace_filtered = SZB.Diag.ZBVars.trace_filtered;
 
-    ζsqT = SZB.Diag.ZBVars.ζsqT;
-    ζDT = SZB.Diag.ZBVars.ζDT;
-    ζDhat = SZB.Diag.ZBVars.ζDhat;
-
-    return T11_true, T12_true, T22_true, ζD_filtered, ζDhat_filtered, trace_filtered, ζsqT, ζDT, ζDhat, T11_NN, T12_NN, T22_NN
+    return T11_true, T12_true, T22_true, ζD_filtered, ζDhat_filtered, trace_filtered, T11_NN, T12_NN, T22_NN, T11_pred, T12_pred, T22_pred
 
 end
 
@@ -160,10 +195,13 @@ PZB = ShallowWaters.Parameter(T=Float64;
 );
 SZB = ShallowWaters.model_setup(PZB);
 
-T11_true, T12_true, T22_true, ζD_filtered, ζDhat_filtered, trace_filtered, ζsqT, ζDT, ζDhat, T11_NN, T12_NN, T22_NN = compute_Ts();
+# snapshot to use, can be anything within a one year integration
+j = 2
+Ndays = 1
+T11_true, T12_true, T22_true, ζD_filtered, ζDhat_filtered, trace_filtered, T11_NN, T12_NN, T22_NN, T11_pred, T12_pred, T22_pred = compute_Ts(j, Ndays);
 denom = SZB.grid.Δ^2 * SZB.grid.scale
 
-## "True" versus NN (offline) T's
+## "True" versus NN T's
 
 fig = Figure(fontsize = 15,size=(900,450));
 
@@ -224,6 +262,132 @@ colormap=:balance,
 axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{22}"),
 colorrange=(-maximum(abs.(T22_true)),
 maximum(abs.(T22_true)))
+);
+Colorbar(fig[2,6], hm3)
+
+# Zanna-Bolton output versus the NN output
+
+fig = Figure(fontsize = 15, size=(900,450));
+ax1, hm1 = heatmap(fig[1,1], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+(trace_filtered - ζD_filtered)./denom,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\zeta^2 - \zeta D"),
+colorrange=((-maximum(abs.((trace_filtered - ζD_filtered)./denom))),
+maximum(abs.((trace_filtered - ζD_filtered)./denom)))
+);
+Colorbar(fig[1,2], hm1)
+
+ax2, hm2 = heatmap(fig[1,3], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+ζDhat_filtered ./ denom,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\zeta \hat{D}"),
+colorrange=(-maximum(abs.(ζDhat_filtered ./ denom)),
+maximum(abs.(ζDhat_filtered ./ denom)))
+);
+Colorbar(fig[1,4], hm2)
+
+ax3, hm3 = heatmap(fig[1,5], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+(trace_filtered + ζD_filtered)./denom,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\zeta^2 + \zeta D"),
+colorrange=(-maximum(abs.((trace_filtered + ζD_filtered)./denom)),
+maximum(abs.((trace_filtered + ζD_filtered)./denom)))
+);
+Colorbar(fig[1,6], hm3)
+
+ax1, hm1 = heatmap(fig[2,1], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T11_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{11}"),
+colorrange=((-maximum(abs.(T11_NN))),
+maximum(abs.(T11_NN)))
+);
+Colorbar(fig[2,2], hm1)
+
+ax2, hm2 = heatmap(fig[2,3], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T12_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{12}"),
+colorrange=(-maximum(abs.(T12_NN)),
+maximum(abs.(T12_NN)))
+);
+Colorbar(fig[2,4], hm2)
+
+ax3, hm3 = heatmap(fig[2,5], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T22_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{22}"),
+colorrange=(-maximum(abs.(T22_NN)),
+maximum(abs.(T22_NN)))
+);
+Colorbar(fig[2,6], hm3)
+
+# Prediction versus the NN output
+
+fig = Figure(fontsize = 15, size=(900,450));
+ax1, hm1 = heatmap(fig[1,1], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T11_pred,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{11}(-)"),
+colorrange=((-maximum(abs.(T11_pred))),
+maximum(abs.(T11_pred)))
+);
+Colorbar(fig[1,2], hm1)
+
+ax2, hm2 = heatmap(fig[1,3], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T12_pred,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{12}(-)"),
+colorrange=(-maximum(abs.(T12_pred)),
+maximum(abs.(T12_pred)))
+);
+Colorbar(fig[1,4], hm2)
+
+ax3, hm3 = heatmap(fig[1,5], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T22_pred,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{22}(-)"),
+colorrange=(-maximum(abs.(T22_pred)),
+maximum(abs.(T22_pred)))
+);
+Colorbar(fig[1,6], hm3)
+
+ax1, hm1 = heatmap(fig[2,1], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T11_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{11}"),
+colorrange=((-maximum(abs.(T11_NN))),
+maximum(abs.(T11_NN)))
+);
+Colorbar(fig[2,2], hm1)
+
+ax2, hm2 = heatmap(fig[2,3], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T12_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{12}"),
+colorrange=(-maximum(abs.(T12_NN)),
+maximum(abs.(T12_NN)))
+);
+Colorbar(fig[2,4], hm2)
+
+ax3, hm3 = heatmap(fig[2,5], LinRange(0, 3840, 128),
+LinRange(0, 3840, 128),
+T22_NN,
+colormap=:balance,
+axis=(xlabel="km", ylabel="km", title=L"\tilde{T}_{22}"),
+colorrange=(-maximum(abs.(T22_NN)),
+maximum(abs.(T22_NN)))
 );
 Colorbar(fig[2,6], hm3)
 
